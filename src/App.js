e22691d7de6ea2c51e93bcb699fcbf3583624cac
jansiteresa <jansiteresa94@gmail.com>
2020-10-22 21:59:12 +0530

    Created Skeleton of Home Page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,16 @@ import React from 'react';
 function App() {
   return (
     <div>
-      Phone Directory
+      {/* <div className="header">
+        Phone Directory
+    </div>
+      <button> Add </button>
+      <div>
+        <span> Name </span> <br />
+        <span> Phone </span>
+      </div> */}
+      <label htmlFor="name">Name: </label>
+      <input type="text" id="name" placeholder="Type Here" defaultValue="Jansi"/>
     </div>
   );
 }
